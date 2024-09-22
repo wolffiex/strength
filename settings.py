f758ce3daf6ec27a013814aa251b2113fa00ddb4
Adam Wolff <adam@reginald>
2024-09-22 08:45:39 -0700

    Selection script

--- settings.py
+++ settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "dist",
+    BASE_DIR / "www",
 ]
 
 # Default primary key field type
---
+++ www/js/selection.js
@@ -0,0 +1,31 @@
+function updateSelection(selection, elements) {
+  for (let i = 0; i < elements.length; i++) {
+    let el = elements[i]
+    if (selection.has(i)) {
+      el.classList.remove("bg-gray-800");
+      el.classList.add("bg-green-500");
+    } else {
+      el.classList.add("bg-gray-800");
+      el.classList.remove("bg-green-500");
+    }
+  }
+}
+
+function makeSelectable(querySelector) {
+  let selection = new Set()
+  let elements = document.querySelectorAll(querySelector)
+  for (let i = 0; i < elements.length; i++) {
+    let el = elements[i]
+    let n = i
+    el.addEventListener("click", function () {
+      if (selection.has(n)) {
+        selection.delete(n)
+      } else {
+        selection.add(n)
+      }
+      updateSelection(selection, elements)
+    })
+  }
+}
+
+console.log('ho', makeSelectable)
